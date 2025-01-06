fda464f0ebaf56eaed84f556f5dc4789e949f1ca
naumov.ga <naumov.ga@gmail.com>
2025-01-06 10:14:42 +0300

    2

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from math import *
-import itertools
 
-def  CalculateSquareRoot (Number ):
-	return  sqrt(Number )
+def CalculateSquareRoot (Number ):
+    return sqrt(Number )
 
-def append_item(item, l=[]):
+
+def append_item(item, l = []):
     l.append(item)
     return l
 
+
 while True:
     try:
-        your_number=float(input('Enter your number: '))
+        your_number = float(input('Enter your number: '))
         print("Square root is:", CalculateSquareRoot(your_number)) 
-        break  
+        break
     except:
         pass
-    
